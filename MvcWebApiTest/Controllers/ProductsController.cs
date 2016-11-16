f9f317fcee5c87a0a113afce33445dcea4e40f33
maparker <maparker@bauer-media.com.au>
2016-11-16 16:40:10 +1100

    html web IMG crawler

--- MvcWebApiTest/Controllers/ProductsController.cs
+++ MvcWebApiTest/Controllers/ProductsController.cs
@@ -2,12 +2,14 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Helpers;
 using System.Web.Http;
+using HtmlAgilityPack;
 
 
 namespace MvcWebApiTest.Controllers
@@ -45,6 +47,54 @@ namespace MvcWebApiTest.Controllers
         public HttpResponseMessage Get(int id)
         {
             return Request.CreateResponse(HttpStatusCode.OK, products);
+        }
+
+        [Route("crawler")]
+        [HttpGet]
+        public HttpResponseMessage Crawler()
+        {
+            String Rstring;
+            WebRequest myWebRequest;
+            WebResponse myWebResponse;
+            String URL = "http://www.londonrentmyhouse.com";
+
+            myWebRequest = WebRequest.Create(URL);
+            myWebResponse = myWebRequest.GetResponse();//Returns a response from an Internet resource
+
+            Stream streamResponse = myWebResponse.GetResponseStream();//return the data stream from the internet
+            //and save it in the stream
+
+            StreamReader sreader = new StreamReader(streamResponse);//reads the data stream
+            Rstring = sreader.ReadToEnd();//reads it to the end
+            String Links = GetContent(Rstring);//gets the links only
+
+            streamResponse.Close();
+            sreader.Close();
+            myWebResponse.Close();
+
+            
+            
+            
+            
+            return Request.CreateResponse(HttpStatusCode.OK, products);
+        }
+
+
+        private String GetContent(String Rstring)
+        {
+            String sString = "";
+            HtmlDocument d = new HtmlDocument();
+
+            d.LoadHtml(Rstring);
+
+            foreach(HtmlNode img in d.DocumentNode.Descendants().Where(n => n.Name == "img")) {
+
+                sString += img.OuterHtml;
+                sString += "/n";
+
+            }
+
+            return sString;
         }
 
 
